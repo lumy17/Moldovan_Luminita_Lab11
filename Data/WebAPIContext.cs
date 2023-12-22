2355480cf489afbec922f67d3c6d7aa9e727c2cf
LUMINIȚA MOLDOVAN <luminita.moldovan1@stud.ubbcluj.ro>
2023-12-22 22:42:19 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Luminita_Lab11.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Luminita_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
---
+++ Models/ShopList.cs
@@ -0,0 +1,9 @@
+﻿namespace Moldovan_Luminita_Lab11.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
